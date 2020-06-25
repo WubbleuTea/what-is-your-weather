e8154d5747be41885275e97ef80997fb4bfa1cb1
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-06-25 13:05:49 -0500

    started javascript

---
+++ assets/js/script.js
@@ -0,0 +1,2 @@
+fetch("api.openweathermap.org/data/2.5/weather?q={city name}&appid=880297035eb22848e2ca9e450c64066f")
+fetch("api.openweathermap.org/data/2.5/forecast?q={city name}&appid=880297035eb22848e2ca9e450c64066f")
